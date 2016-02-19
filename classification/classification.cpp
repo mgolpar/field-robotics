22fbb6681515bdb2ec451f9ef981495430b875c6
Xinke Deng <xinke.ae@gmail.com>
2016-02-19 00:14:25 -0600

    incorporating PCL in classification

--- classification/classification.cpp
+++ classification/classification.cpp
@@ -12,6 +12,9 @@
 #include <ros/ros.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <geometry_msgs/Twist.h> 
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+#include <boost/foreach.hpp>
 // please include all the message packages you want to use
 
 
@@ -31,12 +34,21 @@
 geometry_msgs::Twist msg_pub;
 ros::Publisher pub_classification;
 
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+
+void callback(const PointCloud::ConstPtr& msg)
+{
+     printf ("Cloud: width = %d, height = %d\n", msg->width, msg->height);
+     BOOST_FOREACH (const pcl::PointXYZ& pt, msg->points)
+     printf ("\t(%f, %f, %f)\n", pt.x, pt.y, pt.z);
+}
+
 void callback_Classification (const geometry_msgs::Twist& msg)
 {
 
-  ROS_INFO_STREAM("Sending random velocity command:"
-		  << " linear=" << msg.linear.x
-		  << " angular=" << msg.angular.z);
+  //ROS_INFO_STREAM("Sending random velocity command:"
+			//<< " linear=" << msg.linear.x
+		  //<< " angular=" << msg.angular.z);
   msg_pub = msg;
   
   pub_classification.publish(msg_pub);
@@ -52,6 +64,7 @@ int main(int argc, char* argv[])
 
   pub_classification = nh_class.advertise<geometry_msgs::Twist>("classification/data", 1000);
   ros::Subscriber sub_classification = nh_class.subscribe ("tango/data",1000,&callback_Classification);
+  ros::Subscriber sub_pcl = nh_class.subscribe<PointCloud>("tango/pcl",1000,&callback);
 
   ros::spin();
 
